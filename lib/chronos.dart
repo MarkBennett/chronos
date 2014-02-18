b5a9d4fc213915688e5dc71c6af2b172e8fa4fbd
Mark Bennett <mark@burmis.ca>
2014-02-17 20:57:13 -0700

    Timesheet has a `#starts_at" property

--- lib/chronos.dart
+++ lib/chronos.dart
@@ -1,14 +1,11 @@
 library chronos;
 
 class Timesheet {
-  List<Entry> entries = [];
-
-  Timesheet() {
-    entries = [
-      new Entry(new Duration(hours: 1, minutes: 30), "prototyping UI", "chronos"),
-      new Entry(new Duration(hours: 1), "adding behaviour", "chronos")
-    ];
-  }
+  List<Entry> entries = [
+    new Entry(new Duration(hours: 1, minutes: 30), "prototyping UI", "chronos"),
+    new Entry(new Duration(hours: 1), "adding behaviour", "chronos")
+  ];
+  DateTime starts_at = new DateTime.now();
 }
 
 class Entry {
