3330db4a920031186bde4d3782e6787a26a2589f
Mark Bennett <mark@burmis.ca>
2014-04-08 20:32:03 -0600

    Break chronos models into parts.

---
+++ lib/chronos/entry.dart
@@ -0,0 +1,26 @@
+part of chronos;
+
+class Entry {
+  String id;
+  Duration duration;
+  String description;
+  String client;
+
+  Entry(this.id, this.duration, this.description, this.client);
+
+  Entry.fromJson(Map json) {
+    id = json['id'];
+    duration = new Duration(seconds: json['duration']);
+    description = json['description'];
+    client = json['client'];
+  }
+
+  Map toJson() {
+    return {
+      'id': id,
+      'duration': duration.inSeconds,
+      'description': description,
+      'client': client
+    };
+  }
+}
