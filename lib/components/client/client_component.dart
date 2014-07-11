53620c99ce81fbc978fabdceccd2191322d9c502
Mark Bennett <mark@burmis.ca>
2014-07-10 21:07:47 -0600

    Create client component

---
+++ lib/components/client/client_component.dart
@@ -0,0 +1,12 @@
+library client_component;
+
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'client',
+    templateUrl: 'packages/chronos/components/client/client_component.html',
+    cssUrl: 'packages/chronos/components/client/client_component.css',
+    publishAs: 'ctrl')
+class ClientComponent {
+
+}
--- web/chronos.dart
+++ web/chronos.dart
@@ -6,6 +6,7 @@ import 'package:angular/application_factory.dart';
 @MirrorsUsed(override: '*')
 import 'dart:mirrors';
 
+import 'package:chronos/components/client/client_component.dart';
 import 'package:chronos/components/timesheet/timesheet_component.dart';
 import 'package:chronos/filters/duration_filter.dart';
 import 'package:chronos/resources/timesheets_resource.dart';
@@ -15,6 +16,7 @@ class ChronosModule extends Module {
   ChronosModule() {
     bind(TimesheetResource);
     bind(TimesheetComponent);
+    bind(ClientComponent);
     bind(DurationFilter);
     bind(RouteInitializerFn, toValue: chronosRouteInitializer);
   }
