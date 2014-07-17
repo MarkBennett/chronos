f58a9b41404ac4cb6392a1c8a45f70d926e64270
Mark Bennett <mark@burmis.ca>
2014-07-17 11:27:44 -0600

    Initial changes to get clients from GDrive
    I still need to save them back to GDrive.

--- lib/resources/client_resource.dart
+++ lib/resources/client_resource.dart
@@ -4,44 +4,64 @@ import 'dart:async';
 
 import 'package:angular/angular.dart';
 import 'package:chronos/resources/resource.dart';
+import 'package:chronos/resources/gdrive_adapter.dart';
 
 class Client {
+  String id;
   String name;
 
-  Client(this.name);
+  Client(this.id, this.name);
+  Client.fromJson(json) {
+    this.id = json["id"];
+    this.name = json["name"];
+  }
 }
 
 @Injectable()
 class ClientResource extends Resource {
+  GDriveAdapter adapter;
 
-  List<Client> _clients = new List();
+  List<Client> clients = new List();
+
+  Future _inited;
 
   ClientResource() {
-    _clients = [
-      new Client("Bob"),
-      new Client("Jane")
+    adapter = new GDriveAdapter();
+    adapter.register("clients", (Map json) => new Client.fromJson(json));
+
+    _inited = _initClients();
+
+    clients = [
+      new Client("1", "Bob"),
+      new Client("2", "Jane")
     ];
+  }
+
+  Future _initClients() {
+    return adapter.get("clients").then((clients) {
+      this.clients = clients;
+    });
   }
 
   @override
   Future add(entity) {
-    _clients.add(entity);
+    clients.add(entity);
     return new Future.value(entity);
   }
 
   @override
   Future getAll() {
-    return new Future.value(_clients);
+    return new Future.value(clients);
   }
 
   @override
   Future remove(entity) {
-    _clients.remove(entity);
+    clients.remove(entity);
     return new Future.value(entity);
   }
 
   @override
   Future save() {
-    return new Future.value(_clients);
+    return new Future.value(clients);
   }
 }
